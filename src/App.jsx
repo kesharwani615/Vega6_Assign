b74e763898520be4b167e1acb1fe8b6dc4e482b1
kesharwani615 <kesharwanishivam615@gmail.com>
2024-08-28 11:13:23 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import "./App.css"
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Input_Display from "./Page/Input_Display";
 import Add_Caption from "./Page/Add_Caption";
 
@@ -8,12 +8,12 @@ import Add_Caption from "./Page/Add_Caption";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path="/" element={<Input_Display />} />
       <Route path="/Add_Caption/:id" element={<Add_Caption />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
   )
 }
 
